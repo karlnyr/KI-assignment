56d3481bb0924defb2abf891546de57ddf0e65d4
Karl Nyrén <kallejohan211@hotmail.com>
2020-04-25 00:00:04 +0200

    new setup with requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
+try:
+    with open('requirements.txt', 'r') as fh:
+        install_requires = [line.strip() for line in fh.readlines()]
+except IOError:
+    install_requires = []
+
 setup(
     name='kia',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'seaborn',
-        'pandas',
-    ],
+    install_requires=install_requires,
     entry_points='''
         [console_scripts]
         kia=commands.base:cli
